a2ce3ac6a8fae1a89aed1df3884e7b7aa7fcb51f
itbeginshere <59728343+itbeginshere@users.noreply.github.com>
2022-12-27 14:51:23 +0200

    dialog font size fix

--- components/dialogs/DeleteDialog.tsx
+++ components/dialogs/DeleteDialog.tsx
@@ -14,8 +14,8 @@ const DeleteDialog = (props : IDeleteDialogProps) => {
     
     return (
         <dialog open={open} className={'backdrop:bg-primary fixed rounded-xl top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] border-l-4 border-primary w-[80%]'}>
-            <p className={'text-md md:text-lg'}>Are you sure that you want to delete the following task:</p>
-            <p className={'text-md md:text-lg font-semibold'}>{'Task Name'}</p>
+            <p className={'text-base md:text-lg'}>Are you sure that you want to delete the following task:</p>
+            <p className={'text-base md:text-lg font-semibold'}>{'Task Name'}</p>
             <div className={'flex flex-row justify-end items-center gap-5'}>
                 <button className={'transition rounded-3xl py-2 px-6 border-2 border-primary hover:border-secondary bg-primary hover:bg-secondary hover:shadow-lg hover:translate-y-[-2px]'} onClick={onConfirm}>
                     <span className={'text-white font-semibold'}>YES</span>
