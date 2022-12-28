4dc45836e4b9f77e71283d256e89e3bc5096f9a8
itbeginshere <59728343+itbeginshere@users.noreply.github.com>
2022-12-28 20:45:22 +0200

    new task button bounce

--- components/buttons/NewTaskButton.tsx
+++ components/buttons/NewTaskButton.tsx
@@ -22,7 +22,7 @@ const NewTaskButton = () => {
         <>
             <button 
                 onClick={openDialog}
-                className={'transition rounded-full bg-primary hover:bg-secondary hover:shadow-lg py-3 px-10 w-full md:w-fit'}
+                className={'transition rounded-full bg-primary hover:bg-secondary hover:shadow-lg py-3 px-10 w-full md:w-fit hover:translate-y-[-4px] '}
             >
                 <span className={'text-white font-semibold uppercase'}>
                     New Task
