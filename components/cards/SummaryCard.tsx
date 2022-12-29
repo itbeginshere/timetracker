8ef7e42ed22c1410cf8eb4657da07fabd2beeaf9
itbeginshere <59728343+itbeginshere@users.noreply.github.com>
2022-12-29 19:14:42 +0200

    elapsed time

--- components/cards/SummaryCard.tsx
+++ components/cards/SummaryCard.tsx
@@ -1,22 +1,59 @@
+import moment from 'moment';
+import { useMemo } from 'react';
+import { useAppSelector } from '../../redux/hooks';
+import LoadingIndicator from '../common/LoadingIndicator';
+import ElapsedTime from './summaryCard/ElapsedTime';
+
 const SummaryCard = () => {
+    
+    const tasks = useAppSelector(x => x.taskState.tasks);
+    const isTaskLoading = useAppSelector(x => x.taskState.isLoading);
+    
+    const savedTime = useMemo(() => {
+
+        const totalDuration = tasks.reduce((acc, item) => {
+            return acc + item.duration;
+        }, 0);
+
+        return moment.duration(totalDuration);
+
+    }, [tasks]);
+
+    const completedCount = useMemo(() => {
+        return tasks.filter(x => x.completed).length;
+    }, [tasks]);
+
+    const inProgressCount = useMemo(() => {
+        return tasks.filter(x => !x.completed).length;
+    }, [tasks]);
+
+    const completionPercentage = useMemo(() => {
+        return (completedCount / tasks.length) * 100;
+    },[completedCount, tasks]);
+
     return (
         <div className={'flex flex-col gap-3 bg-white shadow-lg rounded-2xl py-3 px-5 border-b-4 border-secondary w-full'}>
             <div className={'flex flex-row gap-3'}>
                 <span className={'text-lg font-semibold w-[140px]'}>Total Time</span>
-                <span className={'text-lg font-semibold text-secondary'}>60 minutes</span>
+                <ElapsedTime duration={savedTime} />
             </div>
             <div className={'flex flex-row gap-3'}>
                 <span className={'text-lg font-semibold w-[140px]'}>Completed</span>
-                <span className={'text-lg font-semibold text-secondary'}>2</span>
+                <span className={'text-lg font-semibold text-secondary'}>{completedCount}</span>
             </div>
             <div className={'flex flex-row gap-3'}>
                 <span className={'text-lg font-semibold w-[140px]'}>In-Progress</span>
-                <span className={'text-lg font-semibold text-secondary'}>2</span>
+                <span className={'text-lg font-semibold text-secondary'}>{inProgressCount}</span>
             </div>
             <div className={'flex flex-row gap-3'}>
                 <span className={'text-lg font-semibold w-[140px]'}>Completion</span>
-                <span className={'text-lg font-semibold text-secondary'}>50 %</span>
+                <span className={'text-lg font-semibold text-secondary'}>{completionPercentage} %</span>
             </div>
+            {
+                isTaskLoading && (
+                    <LoadingIndicator />
+                )
+            }
         </div>
     );
 }
---
+++ components/cards/summaryCard/ElapsedTime.tsx
@@ -0,0 +1,41 @@
+import moment from 'moment';
+import { useMemo } from 'react';
+
+interface IElapsedTimeProps {
+    duration : moment.Duration;
+}
+
+const ElapsedTime = (props : IElapsedTimeProps) => {
+    
+    const { duration } = props;
+
+    const time = useMemo(() => {
+
+        let result = '';
+
+        if (duration.days() > 0) {
+            result += `${duration.days()} Day${duration.days() > 1 ? 's' : ''} `;
+        }
+
+        if (duration.hours() > 0) {
+            result += `${duration.hours()} Hour${duration.hours() > 1 ? 's' : ''} `;
+        }
+
+        if (duration.minutes() > 0) {
+            result += `${duration.minutes()} Minute${duration.minutes() > 1 ? 's' : ''} `;
+        }
+
+        if (duration.seconds() > 0) {
+            result += `${duration.seconds()} Second${duration.seconds() > 1 ? 's' : ''} `;
+        }
+
+        return result;
+
+    }, [duration]);
+    
+    return (
+        <span className={'text-lg font-semibold text-secondary'}>{time}</span>
+    );
+}
+
+export default ElapsedTime;
