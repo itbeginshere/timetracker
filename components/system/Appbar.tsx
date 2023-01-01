e83c926520b7316fd38705d5a7574afafd00c8be
itbeginshere <59728343+itbeginshere@users.noreply.github.com>
2023-01-01 19:56:26 +0200

    versioning

--- components/system/Appbar.tsx
+++ components/system/Appbar.tsx
@@ -1,11 +1,12 @@
 import LogoSVG from '../svgs/LogoSVG';
+import { version } from '../../package.json';
 
 const Appbar = () => {
     return (
         <header className={'flex flex-row items-center justify-center w-full'}>
             <div className={'flex flex-row items-center bg-primary py-4 px-7 gap-2 rounded-b-3xl shadow-md'}>
                 <LogoSVG fill={'white'}/>
-                <span className={'text-white text-2xl font-semibold'}>TimeTracker</span>
+                <span className={'text-white text-2xl font-semibold'}>TimeTracker v{version}</span>
             </div>
         </header>
     );
